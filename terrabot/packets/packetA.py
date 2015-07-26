30f574727a8dafd89d4a58dc662c7cdc9a64e8bc
Alexander <rubberduck313@gmail.com>
2015-07-26 17:13:41 +0200

    Commit to fix my terminal

--- terrabot/packets/packetA.py
+++ terrabot/packets/packetA.py
@@ -5,17 +5,17 @@ import PIL
 class PacketAParser(object):
 
     def parse(self, world, player, data):
-        print "Section Data"
-        print len(data)
         streamer = Streamer(data)
         streamer.next_byte() #Skip packet number byte
         tiles = []
         compressed = streamer.next_byte()
-        print compressed
+
         if compressed:
             #I dont actually care about compressed packets right now
             #because I dont know how they work yet
             return
+        print "Non-compressed\n"
+
         startx  = streamer.next_int32()
         starty  = streamer.next_int32()
         width   = streamer.next_short()
@@ -33,7 +33,7 @@ class PacketAParser(object):
         for y in range(0, height):
             for x in range(0, width):
                 #Read a tile!
-                flag    = streamer.next_byte()()
+                flag    = streamer.next_byte()
                 active  = flag & 2  > 0
                 flag2   = flag & 1  > 0
                 isShort = flag & 32 > 0
@@ -41,16 +41,16 @@ class PacketAParser(object):
                 color = (0, 0, 0)
 
                 if flag2:
-                    flag2 = streamer.next_byte()()
+                    flag2 = streamer.next_byte()
                     flag3 = flag2 & 1 > 0
                     if flag3:
-                        flag3 = streamer.next_byte()()
+                        flag3 = streamer.next_byte()
                         hasColor = flag3 & 8 > 0
 
                 if isShort:
-                    type = streamer.nextShort()
+                    type = streamer.next_short()
                 else:
-                    type = streamer.next_byte()()
+                    type = streamer.next_byte()
 
                 print type
 
