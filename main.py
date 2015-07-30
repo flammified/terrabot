7997d4808b7ca1faf95a8e311d063545b2d9ac2f
Alexander Freeman <flammified@users.noreply.github.com>
2015-07-30 16:42:23 +0200

    PEP8 in main

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from terrabot import TerraBot
 
-import threading, time
+import threading
 import sys
 import select
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
 
-	HOST = '127.0.0.1'
-	PORT = 7777
-	PROTOCOL = 102
-	PLAYERNAME = 'Terrabot'
+    HOST = '127.0.0.1'
+    PORT = 7777
+    PROTOCOL = 102
+    PLAYERNAME = 'Terrabot'
 
-	if (len(sys.argv) > 1):
-		HOST = sys.argv[1]
-	if (len(sys.argv) > 2):
-		PLAYERNAME = sys.argv[2]
-	if (len(sys.argv) > 3):
-		PORT = sys.argv[3]
+    if len(sys.argv) > 1:
+        HOST = sys.argv[1]
+    if len(sys.argv) > 2:
+        PLAYERNAME = sys.argv[2]
+    if len(sys.argv) > 3:
+        PORT = sys.argv[3]
 
-	bot = TerraBot(HOST, port=PORT, name=PLAYERNAME)
-	bot.start()
-	while threading.active_count() > 1:
-		while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-			line = sys.stdin.readline()
-			line = line.rstrip()
-			if line:
-				if line == "stop":
-					bot.stop()
-				else:
-					print
-					bot.message(line)
+    bot = TerraBot(HOST, port=PORT, name=PLAYERNAME)
+    bot.start()
+    while threading.active_count() > 1:
+       pass
