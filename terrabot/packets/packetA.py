a76620535f354c3eeb5f1243aa53115ae761f399
chieltbest <chieltbest@gmail.com>
2015-09-01 14:49:06 +0200

    fixed packet A, can now receive tiles

--- terrabot/packets/packetA.py
+++ terrabot/packets/packetA.py
@@ -27,89 +27,85 @@ class PacketAParser(object):
         print "StartY: " + str(starty)
         print "Width: " + str(width)
         print "Height: " + str(height)
-        print "ByteLength: " + str(len(streamer.remainder()))
+        #print "ByteLength: " + str(len(streamer.remainder()))
 
-        for i in range(0, height):
+        for i in range(height):
             tiles.append([])
 
         repeat_count = 0
         last_tile = None
 
-        x = 0
-        y = 0
-
-        while y < height:
-            while x < width:
+        for y in range(height):
+            for x in range(width):
                 if repeat_count > 0:
                     repeat_count -= 1
                     tiles[y].append(last_tile)
-                    x += 1
-                    if x > width:
-                        y += 1
-                        x = 0
-                    continue
+                else:
+                    flag = streamer.next_byte()
+                    flag2 = 0
+                    flag3 = 0
+                    if flag & 1:
+                        flag2 = streamer.next_byte()
+                    if flag2 & 1:
+                        flag3 = streamer.next_byte()
 
-                flag = streamer.next_byte()
-                flag2 = streamer.next_byte()
-                flag3 = streamer.next_byte()
+                    active = flag & 2 > 0
+                    has_wall = flag & 4 > 0
+                    liquid = flag & 8 > 0
+                    is_short = flag & 32 > 0
+                    repeat_value_present = flag & 64 > 0
+                    extra_repeat_value = flag & 128 > 0
 
-                active = flag & 2 > 0
-                has_wall = flag & 4 > 0
-                liquid = flag & 8 > 0
-                is_short = flag & 32 > 0
-                repeat_value_present = flag & 64 > 0
-                extra_repeat_value = flag & 128 > 0
+                    print str(int(active)) + " " + str(int(has_wall)) + " " + str(int(liquid)) + " " + str(int(is_short)) + " " + str(int(repeat_value_present)) + " " + str(int(extra_repeat_value))
 
-                if not repeat_value_present and extra_repeat_value:
-                    print "WTF"
+                    if not repeat_value_present and extra_repeat_value:
+                        print "WTF"
 
-                wire = flag2 & 2 > 0
-                wire2 = flag2 & 4 > 0
-                wire3 = flag2 & 8 > 0
+                    wire = flag2 & 2 > 0
+                    wire2 = flag2 & 4 > 0
+                    wire3 = flag2 & 8 > 0
 
-                has_color = flag3 & 8 > 0
-                has_wall_color = flag3 & 16 > 0
+                    has_color = flag3 & 8 > 0
+                    has_wall_color = flag3 & 16 > 0
 
-                frame_y = 0
-                frame_x = 0
-                wall = 0
-                wall_color = 0
-                tile_type = 0
-                color = (0, 0, 0)
+                    frame_y = 0
+                    frame_x = 0
+                    wall = 0
+                    wall_color = 0
+                    tile_type = 0
+                    color = (0, 0, 0)
 
-                if active:
-                    if is_short:
-                        tile_type = streamer.next_short()
-                    else:
-                        tile_type = streamer.next_byte()
-                    if tile_type in frameImportant:
-                        frame_x = streamer.next_short()
-                        frame_y = streamer.next_short()
-                if has_color:
-                    color = streamer.next_byte()
-                if has_wall:
-                    wall = streamer.next_byte()
-                    if has_wall_color:
-                        wall_color = streamer.next_byte()
-                if liquid:
-                    streamer.next_byte()
-                if wire:
-                    streamer.next_byte()
-                if wire2:
-                    streamer.next_byte()
-                if wire3:
-                    streamer.next_byte()
-                if repeat_value_present:
+                    if active:
+                        if is_short:
+                            tile_type = streamer.next_short()
+                        else:
+                            tile_type = streamer.next_byte()
+                        if tile_type in frameImportant:
+                            frame_x = streamer.next_short()
+                            frame_y = streamer.next_short()
+                    if has_color:
+                        color = streamer.next_byte()
+                    if has_wall:
+                        wall = streamer.next_byte()
+                        if has_wall_color:
+                            wall_color = streamer.next_byte()
+                    if liquid:
+                        streamer.next_byte()
+                    if wire:
+                        streamer.next_byte()
+                    if wire2:
+                        streamer.next_byte()
+                    if wire3:
+                        streamer.next_byte()
                     if extra_repeat_value:
                         repeat_count = streamer.next_short()
                     else:
-                        repeat_count = streamer.next_byte()
-                temp_tile = Tile()
-                last_tile = temp_tile
-                tiles[y].append(temp_tile)
-                x += 1
-            y += 1
-            x = 0
+                        if repeat_value_present:
+                            repeat_count = streamer.next_byte()
+                    # print str(frame_x) + " " + str(frame_y) + " " + str(wall) + " " + str(wall_color) + " " + str(tile_type) + " " + str(color) + " " + str(repeat_count)
+                    temp_tile = Tile()
+                    last_tile = temp_tile
+                    tiles[y].append(temp_tile)
             # print str(len(streamer.remainder()))
             # print "XY: " + str(x) + " : " + str(y) + " Rem: " + str(len(streamer.remainder()))
         # print "Width: " + str(len(tiles[0]))
