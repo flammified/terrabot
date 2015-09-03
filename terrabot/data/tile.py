c3b29d28d6920b6cfe0b321e10c4ebd42d6f79a6
Alexander Freeman <Rubberduck313@gmail.com>
2015-09-03 10:06:11 +0200

    Initialized the tile array

--- terrabot/data/tile.py
+++ terrabot/data/tile.py
@@ -1,8 +1,6 @@
-
-
 # Temporary class, will be moved to own file later on
 class Tile(object):
-    def __init__(self, active=True, tile_type=0, color=(0, 0, 0)):
+    def __init__(self, tile_type=0, active=True, color=(0, 0, 0)):
         self.active = active
         self.type = tile_type
         self.color = color
