c94cbca91453d5b18d8b08c73dbcb6169661c39c
Alexander Freeman <Rubberduck313@gmail.com>
2015-09-03 09:07:00 +0200

    Converted array syntax to Streamer

--- terrabot/packets/packet7.py
+++ terrabot/packets/packet7.py
@@ -1,13 +1,16 @@
 import struct
+from terrabot.util.streamer import Streamer
 
 class Packet7Parser(object):
 
 	def parse(self, world, player, data):
-		world.time = struct.unpack("<i", data[1:5])[0]
-		world.daynight = ord(data[5])
-		world.moonphase = ord(data[6])
-		world.maxX = struct.unpack("<h", data[7:9])[0]
-		world.maxY = struct.unpack("<h", data[9:11])[0]
-		world.spawnX = struct.unpack("<h", data[11:13])[0]
-		world.spawnY = struct.unpack("<h", data[13:15])[0]
+		streamer = Streamer(data)
+		streamer.next_byte() # Ignore packet ID byte
+		world.time = streamer.next_int32()
+		world.daynight = streamer.next_byte()
+		world.moonphase = streamer.next_byte()
+		world.maxX = streamer.next_short()
+		world.maxY = streamer.next_short()
+		world.spawnX = streamer.next_short()
+		world.spawnY = streamer.next_short()
 		print str(world.maxX) + " " + str(world.maxY)
