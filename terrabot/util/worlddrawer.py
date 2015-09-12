d6a6d4737a89c6d47cc4454e87a6208da54b9864
Alexander Freeman <Rubberduck313@gmail.com>
2015-09-12 18:14:50 +0200

    Added a progressbar to the world drawer

--- terrabot/util/worlddrawer.py
+++ terrabot/util/worlddrawer.py
@@ -1,11 +1,13 @@
 from PIL import Image, ImageDraw
+from progressbar import *
 
 def draw_world(world):
     image = Image.new("RGB", (world.maxX, world.maxY), "white")
     imgdraw = ImageDraw.Draw(image)
+    widgets = ['Percentage: ', Percentage(), ' ', Bar(marker=RotatingMarker()),
+           ' ', ETA(), ' Speed: ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=len(world.tiles)).start()
 
-    print(len(world.tiles))
-    print(len(world.tiles[0]))
     x = 0
     y = 0
     for j in world.tiles:
@@ -17,10 +19,10 @@ def draw_world(world):
                     color = "rgb(" + str(i.type) + "," + str(i.type) + "," + str(i.type) + ")"
                 imgdraw.point((x, y), fill=color)
             except Exception as e:
-                print(str(e))
-                print(str(x) + " " + str(y))
+                pbar.finish()
             x += 1
         x = 0
         y += 1
-
+        pbar.update(y)
+    pbar.finish()
     image.show()
