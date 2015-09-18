34e5a86f0613adb449bca8c083354437df622a0a
Alexander Freeman <Rubberduck313@gmail.com>
2015-09-18 20:44:17 +0200

    First look at item-related packets

--- terrabot/packets/packet16.py
+++ terrabot/packets/packet16.py
@@ -1,6 +1,9 @@
-import struct
+from terrabot.util.streamer import Streamer
 
 class Packet16Parser(object):
 
 	def parse(self, world, player, data, ev_man):
-		pass
+		streamer = Streamer(data)
+		streamer.next_byte() # Ignore packet number
+		item_id = streamer.next_short()
+		owner_id = streamer.next_byte()
