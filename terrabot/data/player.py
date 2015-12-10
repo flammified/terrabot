0cb8f888819648edac5b5094cf8bad1034d8e42d
Alexander Freeman <rubberduck313@gmail.com>
2015-12-10 12:38:47 +0100

    Revert "Renamed variables to PEP compliant names"
    This reverts commit 02dd85cca4d6eefdf256de0cec98f0f6b62e3747.

--- terrabot/data/player.py
+++ terrabot/data/player.py
@@ -8,21 +8,22 @@ class Player(object):
         self.playerID = 0
         self.hairStyle = 0x0
         self.gender = 1
-        self.hair_color = (255, 255, 255)
-        self.skin_color = (255, 255, 255)
-        self.eye_color = (255, 255, 255)
-        self.shirt_color = (255, 255, 255)
-        self.undershirt_color = (255, 255, 255)
-        self.pants_color = (255, 255, 255)
-        self.shoe_color = (255, 255, 255)
+        self.hairColor = (255, 255, 255)
+        self.skinColor = (255, 255, 255)
+        self.eyeColor = (255, 255, 255)
+        self.shirtColor = (255, 255, 255)
+        self.undershirtColor = (255, 255, 255)
+        self.pantsColor = (255, 255, 255)
+        self.shoeColor = (255, 255, 255)
         self.difficulty = 0
         self.name = name
 
-        self.max_hp = 400
+
+        self.maxHP = 400
         self.currHP = 300
 
-        self.max_mana = 50
-        self.curr_mana = 10
+        self.maxMana = 50
+        self.currMana = 10
 
         self.initialized = False
         self.logged_in = False
