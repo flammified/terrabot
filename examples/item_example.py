fe7485668166e6fae90d79e472a7665e8cf73768
Alexander Freeman <rubberduck313@gmail.com>
2015-12-17 11:14:03 +0100

    Moved item_example to the right folder

---
+++ examples/item_example.py
@@ -0,0 +1,14 @@
+from terrabot import TerraBot
+from terrabot.events import Events
+
+bot = TerraBot('127.0.0.1')
+event = bot.get_event_manager()
+
+@event.on_event(Events.ItemOwnerChanged)
+def logged_in(event_id, data):
+    print(data)
+
+bot.start()
+
+while bot.running:
+    pass
