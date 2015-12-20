bc6ae409363424a7a3f3d2d5f1a252ccd5306e78
Alexander Freeman <rubberduck313@gmail.com>
2015-12-20 20:16:24 +0100

    Added documentation in events

--- terrabot/events/events.py
+++ terrabot/events/events.py
@@ -1,12 +1,38 @@
 import enum
 
 class Events(enum.Enum):
+    #Trigger: chat message
+    #Data: message as string
     Chat = 0
+
+    #Trigger: Tile information
+    #Data: 2D array of Tiles
     TileUpdate = 1
+
+    #Trigger: Login
+    #Data: null
     Login = 2
+
+    #Trigger: PlayerID packet comes in
+    #Data: PlayerID as int
     PlayerID = 3
+
+    #Trigger: Access blocked
+    #Data: reason-message as string
     Blocked = 4
+
+    #Trigger: Initialization on the server
+    #Data: null
     Initialized = 5
+
+    #Trigger: Item owner changes
+    #Data: Tuple: (item_id, owner_id)
     ItemOwnerChanged = 6
+
+    #Trigger: Server sends update on item
+    #Data: Item object
     ItemDropUpdate = 7
+
+    #Trigger: A new player joins
+    #Data: PlayerID
     NewPlayer = 8
