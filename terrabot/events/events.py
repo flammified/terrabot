483a998357d86e2560bb8a9a0fb21843a8b2e283
Alexander Freeman <alexander.freeman.nl@gmail.com>
2017-02-19 00:19:50 +0100

    PEP8 spacing in events.py

--- terrabot/events/events.py
+++ terrabot/events/events.py
@@ -1,42 +1,43 @@
 import enum
 
+
 class Events(enum.Enum):
-    #Trigger: chat message
-    #Data: message as string
+    # Trigger: chat message
+    # Data: message as string
     Chat = 0
 
-    #Trigger: Tile information
-    #Data: 2D array of Tiles
+    # Trigger: Tile information
+    # Data: 2D array of Tiles
     TileUpdate = 1
 
-    #Trigger: Login
-    #Data: null
+    # Trigger: Login
+    # Data: null
     Login = 2
 
-    #Trigger: PlayerID packet comes in
-    #Data: PlayerID as int
+    # Trigger: PlayerID packet comes in
+    # Data: PlayerID as int
     PlayerID = 3
 
-    #Trigger: Access blocked
-    #Data: reason-message as string
+    # Trigger: Access blocked
+    # Data: reason-message as string
     Blocked = 4
 
-    #Trigger: Initialization on the server
-    #Data: null
+    # Trigger: Initialization on the server
+    # Data: null
     Initialized = 5
 
-    #Trigger: Item owner changes
-    #Data: Tuple: (item_id, owner_id)
+    # Trigger: Item owner changes
+    # Data: Tuple: (item_id, owner_id)
     ItemOwnerChanged = 6
 
-    #Trigger: A new item drops
-    #Data: Item object
+    # Trigger: A new item drops
+    # Data: Item object
     ItemDropped = 7
 
-    #Trigger: Server sends update on an item
-    #Data: Item object
+    # Trigger: Server sends update on an item
+    # Data: Item object
     ItemDropUpdate = 8
 
-    #Trigger: A new player joins
-    #Data: PlayerID
+    # Trigger: A new player joins
+    # Data: PlayerID
     NewPlayer = 9
